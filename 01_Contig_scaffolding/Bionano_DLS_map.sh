a855d52f8f4f8d32019c8bde3299609b33a2263e
DESKTOP-3N9C1KP\xiaopeng <1957710158@qq.com>
2023-07-06 15:25:05 +0800

    no message

--- 01_Contig_scaffolding/Bionano_DLS_map.sh
+++ 01_Contig_scaffolding/Bionano_DLS_map.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 threads=$1
+bnx=$2
+ref_cmap=$3
+prefix=$4
+xml=$5
+Bio_dir=$6
+cluster_xml=$7
+ref=$8
+bio_camp=$9
+merge_xml=$10
+RefAligner=$11
 
-
-python pipelineCL.py -Tn $threads -i 5 -b /home/liusc/proj/wheat/rawdata/bionano/output.bnx -r wheat.all.asm.p_ctg_50k_CTTAAG_0kb_0labels.cmap -l all_p50 -e w -a /home/liusc/proj/wheat/result/bionano/optArguments_nonhaplotype_noES_noCut_BG_DLE1_saphyr.xml -t /home/bionano/tools/pipeline/1.0/RefAligner/1.0 -y -z --species-reference other -C /home/bionano/tools/pipeline/1.0/Pipeline/1.0/clusterArguments_saphyr.xml -F 1
-perl /home/bionano/tools/pipeline/1.0/HybridScaffold/1.0/hybridScaffold.pl -n /home/liusc/proj/wheat/result/hifiasm/hifi.20.30x/wheat.asm.p_ctg.fa -b /home/liusc/proj/wheat/result/bionano/all_p50/contigs/w_refineFinal1/W_REFINEFINAL1.cmap -u CTTAAG -c /home/bionano/tools/pipeline/1.0/HybridScaffold/1.0/hybridScaffold_DLE1_config.xml -r /home/bionano/tools/pipeline/1.0/RefAligner/1.0/RefAligner -o bio_hybrid -B 2 -N 2 -f
+python pipelineCL.py -Tn $threads -i 5 -b $bnx -r $ref_cmap -l $prefix -e w -a $xml -t $Bio_dir -y -z --species-reference other -C $cluster_xml -F 1
+perl hybridScaffold.pl -n $ref -b $bio_camp -u CTTAAG -c $merge_xml -r $RefAligner -o bio_hybrid -B 2 -N 2 -f
