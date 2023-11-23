eee9606eb78bec51f8a15369217756cfe20bdcb2
DESKTOP-3N9C1KP\xiaopeng <1957710158@qq.com>
2023-11-23 14:22:48 +0800

    no message

--- 02_Gap_patching/wfmash_ragtag.sh
+++ 02_Gap_patching/wfmash_ragtag.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-prefix=$1
+query=$1
 ref=$2
 region=$3
 mkdir "$region"
 cd "$region"
-wfmash "$ref" "$prefix".fa > "$region".paf 
+wfmash "$ref" "$query" > "$region".paf 
 mkdir ragtag_output
 cd ragtag_output
 ln -s ../"$region".paf ragtag.patch.asm.paf
 cd ..
 ln -s "$ref" ref.fasta
-ln -s "$prefix".fa query.fasta
+ln -s "$query" query.fasta
 
 ragtag.py patch -f 10000 --remove-small ref.fasta query.fasta
 
