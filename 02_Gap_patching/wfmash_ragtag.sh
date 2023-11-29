af969f24a89bb66969d272baaca298d269e6dc5a
DESKTOP-3N9C1KP\xiaopeng <1957710158@qq.com>
2023-11-29 15:41:04 +0800

    no message

--- 02_Gap_patching/wfmash_ragtag.sh
+++ 02_Gap_patching/wfmash_ragtag.sh
@@ -13,6 +13,5 @@ cd ..
 ln -s "$ref" ref.fasta
 ln -s "$query" query.fasta
 
-ragtag.py patch -f 10000 --remove-small ref.fasta query.fasta
-
+ragtag.py patch -i 0.99 --remove-small -q 10 --debug -u --aligner minimap2 -t 128 --mm2-params "-x asm20 -I1G -t 128" ref.fasta query.fasta
 _submit_telomere.sh ragtag_output/ragtag.patch.fasta #(https://github.com/VGP/vgp-assembly)
