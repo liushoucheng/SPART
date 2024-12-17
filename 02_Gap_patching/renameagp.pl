8edd1aab35949d6733957febfd826c45210a74ce
DESKTOP-3N9C1KP\xiaopeng <1957710158@qq.com>
2024-12-17 16:20:12 +0800

    no message

--- 02_Gap_patching/renameagp.pl
+++ 02_Gap_patching/renameagp.pl
@@ -9,9 +9,9 @@ use strict;
 use diagnostics;
 use warnings;
 use Getopt::Long;
-# perl renameagp.pl -i ragtag.patch.rename.agp -i1 ragtag.patch.ctg.agp -i2 gap.agp -i3 ragtag.patch.agp -o gap.rename.agp
+# perl /home/liusc/proj/wheat/code/agp.pl -i ragtag.patch.ctg.agp -i1 ragtag.patch.debug.filtered.paf -start seq00000000 -end seq00000001 -o test.agp
 my %opts=();
-GetOptions(\%opts,"i:s","i1:s","o:s","i2:s","i3:s");
+GetOptions(\%opts,"i:s","i1:s","o:s","start:s","end:s");
 
 if (!$opts{i} or !$opts{o}){
     print "----------------------------------------------------------------------
@@ -20,57 +20,104 @@ if (!$opts{i} or !$opts{o}){
         -o out file
 ----------------------------------------------------------------------\n";
     exit;
-
+    
 }
 my %h=();
 open IN, $opts{i} or die "Cannot open file: $opts{i}!\n";
 open IN1, $opts{i1} or die "Cannot open file: $opts{i1}!\n";
-open IN2, $opts{i2} or die "Cannot open file: $opts{i2}!\n";
-open IN3, $opts{i3} or die "Cannot open file: $opts{i3}!\n";
 open OUT,">$opts{o}" or die "Cannot create file: $opts{o}!\n";
-while(<IN>){
-#qseq00000000    1       209209621       1       W       contig_10       1       209209621       +
+L:while(<IN>){
+#chr8	1	1000000	1	W	seq00000000	1	1000000	+
+#chr8	1000001	1000100	2	N	100	scaffold	yes	align_genus
+#chr8	1000101	4999101	3	W	seq00000001	1	3999001	+
+#scf0	1000001	1000999	2	W	qseq00000000	10002	11000	+
  chomp;
+ if ($_=~ $opts{start}) {
 	  my @f = split /\t/, $_;
 
-  $h{$f[0]}=$f[5];
+  $h{$f[5]}=[@f];
+  @f = split /\t/, <IN>;
+  $h{"scaffold"}=[@f];
+  @f = split /\t/, <IN>;
+  $h{$f[5]}=[@f];
+  last L;
  }
-my $n=0;
-my %h2;
+  }
+    my $start;
+    my $end;
+	my $len2;
 while(<IN1>){
-#chr1A_RagTag_MOD_MOD    1       2046621 1       W       seq00000001     1       2046621 +
- chomp;
-	  my @f = split /\t/, $_;
-    if ($f[0]=~/hr/){
+#qseq00000269	12474565	2150891	12471563	+	seq00000001	79845467	259191	10579680	9480999	10320756	60
+#qseq00000269	12474565	75	2194153	+	seq00000000	673241388	671105857	673241383	2029712	2194088	60
+my $l1=chomp($_);
+  my $number=0;
+  my @f = split /\t/, $_;
 
-  if (!exists $h2{$f[0]}){
-                $h{$f[5]}=$f[0];
+  if ($f[4] eq "+") {
+	  if ($f[5] eq $opts{start}) {
+		my $offset=$f[6]-$f[8];
+		$h{$f[5]}[2]-=$offset;
+		$h{$f[5]}[7]-=$offset;
+		
+		$h{"scaffold"}[1]-=$offset;
+		$start=$f[3];
+		$h{"scaffold"}[1]=$h{$opts{start}}[2]+1;
+		$h{"scaffold"}[6]=$f[3]+1;
+	  }elsif ($f[5] eq $opts{end}){
+		$end=$f[2];
+		  $h{"scaffold"}[4]="W";
+		  $h{"scaffold"}[5]=$f[0];
+		  
+		  $h{"scaffold"}[8]="+";
+		  $h{$opts{end}}[6]=$f[7]+1;
+	  }
+	  
+  }elsif($f[4] eq "-") {
+	  if ($f[5] eq $opts{start}) {
+		  my $offset=$f[6]-$f[8];
+		  $h{$f[5]}[2]-=$offset;
+		  $h{$f[5]}[7]-=$offset;
+		  $h{"scaffold"}[1]-=$offset;
+		  $end=$f[2];
+		  $h{"scaffold"}[1]=$h{$opts{start}}[2]+1;
+		  $h{"scaffold"}[7]=$f[2];
 
-                $h2{$f[0]}=$f[5];
 
-        }
+  }elsif ($f[5] eq $opts{end}){
+		$start=$f[3];
+		  $h{"scaffold"}[4]="W";
+		  $h{"scaffold"}[5]=$f[0];
+		  
+		  $h{"scaffold"}[8]="-";
+		  $h{$f[5]}[6]=$f[7]+1;
+		  $h{"scaffold"}[6]=$f[3]+1;
+	  }
+  }
+}
+  		  my $gapsize=$end-$start;
+		  print $gapsize."\n";
+		  $h{"scaffold"}[2]=$h{"scaffold"}[1]+$gapsize-1;
+		  $h{"scaffold"}[7]=$h{"scaffold"}[6]+$gapsize-1;
+if ($h{"scaffold"}[2]>$h{$opts{start}}[2]) {
 
-    }
-
- }
-while(<IN3>){
-#scf00000000     1       2046621 1       W       seq00000001     1       2046621 +
- chomp;
-	  my @f = split /\t/, $_;
-    if (exists $h2{$h{$f[5]}}){
-         $h{$f[0]}=$h{$f[5]};
-          print $h{$f[5]}."\n";
-
-    }
-
- }
-while(<IN2>){
-#scf00000001     457330265       457811306       10      W       qseq00000182
- chomp;
-	  my @f = split /\t/, $_;
-
-  $f[0]=$h{$f[0]};
-     $f[5]=$h{$f[5]};
-    my $string = join "\t", @f;
-    print OUT $string."\n";
- }
+		$h{$opts{end}}[1]=$h{"scaffold"}[2]+1;
+		$h{$opts{end}}[2]=$h{$opts{end}}[1]+($h{$opts{end}}[7]-$h{$opts{end}}[6]);
+		my $string = join "\t", @{$h{$opts{start}}};
+		print OUT "## agp-version 2.1\n";
+		print OUT "# AGP created by RagTag v2.1.0\n";
+		print OUT $string."\n";
+		$string = join "\t", @{$h{"scaffold"}};
+		print OUT $string."\n";
+		$string = join "\t", @{$h{$opts{end}}};
+		print OUT $string;
+}else{
+		$h{$opts{end}}[1]=$h{$opts{start}}[2]+1;
+		$h{$opts{end}}[2]=$h{$opts{end}}[1]+($h{$opts{end}}[7]-$h{$opts{end}}[6]);
+		$h{$opts{end}}[3]--;
+		my $string = join "\t", @{$h{$opts{start}}};
+		print OUT "## agp-version 2.1\n";
+		print OUT "# AGP created by RagTag v2.1.0\n";
+		print OUT $string."\n";
+		$string = join "\t", @{$h{$opts{end}}};
+		print OUT $string;
+		}
