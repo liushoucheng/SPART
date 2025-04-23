9f9e4be9c5c13dbe182fe5feb0bac3054e6e36f2
liusc <13634831+liushoucheng@users.noreply.github.com>
2025-04-23 09:37:24 +0800

    Update gemma_los.py

--- 00_Contig_screen/gemma_los.py
+++ 00_Contig_screen/gemma_los.py
@@ -1,41 +1,41 @@
-import os
-import re
-import sys
+import argparse
 
-i = 0
-b = 0
-c = []
-c1 = []
-d = {}
-f = {}
-d1 = {}
-d2 = {}
-files = sys.argv[1]
-a=0
-c=0
-with open(files) as file_object:
+# 参数解析
+parser = argparse.ArgumentParser(description="Filter config names based on fixed total length threshold.")
+parser.add_argument("input_file", help="Path to the input TSV file")
+parser.add_argument("--length", type=int, required=True, help="Threshold length to  aligned printing")
+
+args = parser.parse_args()
+
+input_file = args.input_file
+length_threshold = args.length
+
+a = 0
+c = 0
+
+with open(input_file) as file_object:
     for line in file_object:
-        line1 = line.split("\n")
-        line2 = line1[0].split("\t")
-        if a>0:
-            if line2[0]==config_name:
-                d_value=d_value+int(line2[3]) - int(line2[2])
-                if c == 0:
-                    if int(line2[1])/2 < d_value:
-                        print(config_name)
-                        c=1
+        line = line.strip()
+        if not line:
+            continue
+        line2 = line.split("\t")
+        if a > 0:
+            if line2[0] == config_name:
+                d_value += int(line2[3]) - int(line2[2])
+                if c == 0 and d_value > length_threshold:
+                    print(config_name)
+                    c = 1
             else:
                 config_name = line2[0]
                 d_value = int(line2[3]) - int(line2[2])
-                c=0
-                if c == 0:
-                    if int(line2[1]) / 2 < d_value:
-                        print(config_name)
-                        c=1
+                c = 0
+                if d_value > length_threshold:
+                    print(config_name)
+                    c = 1
         else:
-            a=a+1
-            config_name=line2[0]
-            d_value=int(line2[3])-int(line2[2])
-            if int(line2[1])/2 < d_value:
+            a += 1
+            config_name = line2[0]
+            d_value = int(line2[3]) - int(line2[2])
+            if d_value > length_threshold:
                 print(config_name)
-                c=1
+                c = 1
